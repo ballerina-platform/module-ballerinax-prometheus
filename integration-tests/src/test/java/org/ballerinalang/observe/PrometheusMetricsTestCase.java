44ef1ce87864b38ef90b0643be8857445d781289
Nadun De Silva <nadunrds@gmail.com>
2020-10-20 23:15:49 +0530

    Add test case for when Prometheus is disabled

--- integration-tests/src/test/java/org/ballerinalang/observe/PrometheusMetricsTestCase.java
+++ integration-tests/src/test/java/org/ballerinalang/observe/PrometheusMetricsTestCase.java
@@ -20,14 +20,16 @@ package org.ballerinalang.observe;
 import org.ballerinalang.test.context.BServerInstance;
 import org.ballerinalang.test.util.HttpClientRequest;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
@@ -42,73 +44,73 @@ import static io.ballerina.runtime.observability.ObservabilityConstants.CONFIG_T
  * Integration test for Prometheus extension.
  */
 public class PrometheusMetricsTestCase extends BaseTestCase {
-    private static BServerInstance serverInstance;
+    private BServerInstance serverInstance;
 
     private static final File RESOURCES_DIR = Paths.get("src", "test", "resources", "bal").toFile();
     private static final Pattern PROMETHEUS_METRIC_VALUE_REGEX = Pattern.compile("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?");
     private static final String PROMETHEUS_METRICS_URL = "http://localhost:9797/metrics";
     private static final String TEST_RESOURCE_URL = "http://localhost:9091/prometheus-test/sum";
 
-    @BeforeTest
+    @BeforeMethod
     private void setup() throws Exception {
         serverInstance = new BServerInstance(balServer);
     }
 
-    @AfterTest
+    @AfterMethod
     private void cleanUpServer() throws Exception {
         serverInstance.shutdownServer();
     }
 
     @Test
-    public void testMetrics() throws Exception {
+    public void testPrometheusMetrics() throws Exception {
         final Map<String, Pattern> expectedMetrics = new HashMap<>();
         expectedMetrics.put("requests_total_value{service=\"metricsTest\",connector_name=\"http\"," +
                 "src_module=\"_anon/.:0.0.0\",src_entry_point_resource=\"true\",protocol=\"http\"," +
-                "http_url=\"/prometheus_test/sum\",src_position=\"01_prometheus_test.bal:28:5\"," +
-                "resource=\"testCase\",http_method=\"GET\",}",
+                "http_url=\"/prometheus_test/sum\",resource=\"testCase\",src_position=\"01_http_svc_test.bal:28:5\"," +
+                "http_method=\"GET\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
-        expectedMetrics.put("requests_total_value{service=\"metricsTest\",src_module=\"_anon/.:0.0.0\"," +
-                "http_status_code_group=\"2xx\",src_remote=\"true\",action=\"respond\"," +
-                "src_position=\"01_prometheus_test.bal:34:20\",resource=\"testCase\"," +
+        expectedMetrics.put("requests_total_value{service=\"metricsTest\"," +
+                "src_position=\"01_http_svc_test.bal:34:20\",src_module=\"_anon/.:0.0.0\"," +
+                "http_status_code_group=\"2xx\",src_remote=\"true\",action=\"respond\",resource=\"testCase\"," +
                 "connector_name=\"ballerina/http/Caller\",}",
-                PROMETHEUS_METRIC_VALUE_REGEX);
-        expectedMetrics.put("inprogress_requests_value{service=\"metricsTest\",src_module=\"_anon/.:0.0.0\"," +
-                "src_remote=\"true\",action=\"respond\",src_position=\"01_prometheus_test.bal:34:20\"," +
-                "resource=\"testCase\",connector_name=\"ballerina/http/Caller\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
         expectedMetrics.put("inprogress_requests_value{service=\"metricsTest\",connector_name=\"http\"," +
                 "src_module=\"_anon/.:0.0.0\",src_entry_point_resource=\"true\",protocol=\"http\"," +
-                "http_url=\"/prometheus_test/sum\",src_position=\"01_prometheus_test.bal:28:5\"," +
-                "resource=\"testCase\",http_method=\"GET\",}",
+                "http_url=\"/prometheus_test/sum\",resource=\"testCase\",src_position=\"01_http_svc_test.bal:28:5\"," +
+                "http_method=\"GET\",}",
+                PROMETHEUS_METRIC_VALUE_REGEX);
+        expectedMetrics.put("inprogress_requests_value{service=\"metricsTest\"," +
+                "src_position=\"01_http_svc_test.bal:34:20\",src_module=\"_anon/.:0.0.0\",src_remote=\"true\"," +
+                "action=\"respond\",resource=\"testCase\",connector_name=\"ballerina/http/Caller\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
         expectedMetrics.put("response_time_nanoseconds_total_value{service=\"metricsTest\"," +
                 "connector_name=\"http\",src_module=\"_anon/.:0.0.0\",src_entry_point_resource=\"true\"," +
-                "protocol=\"http\",http_url=\"/prometheus_test/sum\",src_position=\"01_prometheus_test.bal:28:5\"," +
-                "resource=\"testCase\",http_method=\"GET\",}",
+                "protocol=\"http\",http_url=\"/prometheus_test/sum\",resource=\"testCase\"," +
+                "src_position=\"01_http_svc_test.bal:28:5\",http_method=\"GET\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
         expectedMetrics.put("response_time_nanoseconds_total_value{service=\"metricsTest\"," +
-                "src_module=\"_anon/.:0.0.0\",http_status_code_group=\"2xx\",src_remote=\"true\"," +
-                "action=\"respond\",src_position=\"01_prometheus_test.bal:34:20\",resource=\"testCase\"," +
-                "connector_name=\"ballerina/http/Caller\",}",
-                PROMETHEUS_METRIC_VALUE_REGEX);
-        expectedMetrics.put("response_time_seconds_value{service=\"metricsTest\",src_module=\"_anon/.:0.0.0\"," +
-                "http_status_code_group=\"2xx\",src_remote=\"true\",action=\"respond\"," +
-                "src_position=\"01_prometheus_test.bal:34:20\",resource=\"testCase\"," +
+                "src_position=\"01_http_svc_test.bal:34:20\",src_module=\"_anon/.:0.0.0\"," +
+                "http_status_code_group=\"2xx\",src_remote=\"true\",action=\"respond\",resource=\"testCase\"," +
                 "connector_name=\"ballerina/http/Caller\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
         expectedMetrics.put("response_time_seconds_value{service=\"metricsTest\",connector_name=\"http\"," +
                 "src_module=\"_anon/.:0.0.0\",src_entry_point_resource=\"true\",protocol=\"http\"," +
-                "http_url=\"/prometheus_test/sum\",src_position=\"01_prometheus_test.bal:28:5\"," +
-                "resource=\"testCase\",http_method=\"GET\",}",
+                "http_url=\"/prometheus_test/sum\",resource=\"testCase\",src_position=\"01_http_svc_test.bal:28:5\"," +
+                "http_method=\"GET\",}",
+                PROMETHEUS_METRIC_VALUE_REGEX);
+        expectedMetrics.put("response_time_seconds_value{service=\"metricsTest\"," +
+                "src_position=\"01_http_svc_test.bal:34:20\",src_module=\"_anon/.:0.0.0\"," +
+                "http_status_code_group=\"2xx\",src_remote=\"true\",action=\"respond\",resource=\"testCase\"," +
+                "connector_name=\"ballerina/http/Caller\",}",
                 PROMETHEUS_METRIC_VALUE_REGEX);
 
         final String[] runtimeArgs = new String[]{
                 "--" + CONFIG_METRICS_ENABLED + "=true",
                 "--" + CONFIG_TABLE_METRICS + ".statistic.percentiles=0.5, 0.75, 0.98, 0.99, 0.999"
         };
-        final String balFile = Paths.get(RESOURCES_DIR.getAbsolutePath(), "01_prometheus_test.bal").toFile()
+        final String balFile = Paths.get(RESOURCES_DIR.getAbsolutePath(), "01_http_svc_test.bal").toFile()
                 .getAbsolutePath();
-        serverInstance.startServer(balFile, null, runtimeArgs, new int[] { 9091 });
+        serverInstance.startServer(balFile, null, runtimeArgs, new int[] { 9091, 9797 });
 
         // Send requests to generate metrics
         int i = 0;
@@ -121,9 +123,13 @@ public class PrometheusMetricsTestCase extends BaseTestCase {
 
         // Read metrics from Prometheus endpoint
         URL metricsEndPoint = new URL(PROMETHEUS_METRICS_URL);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(metricsEndPoint.openConnection()
-                .getInputStream()));
-        List<String> metricsList = reader.lines().filter(s -> !s.startsWith("#")).collect(Collectors.toList());
+        List<String> metricsList;
+        InputStream inputStream = metricsEndPoint.openConnection().getInputStream();
+        try (InputStreamReader isReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            try (BufferedReader bufferedReader = new BufferedReader(isReader)) {
+                metricsList = bufferedReader.lines().filter(s -> !s.startsWith("#")).collect(Collectors.toList());
+            }
+        }
 
         Assert.assertTrue(metricsList.size() != 0);
         int count = 0;
@@ -140,5 +146,15 @@ public class PrometheusMetricsTestCase extends BaseTestCase {
             }
         }
         Assert.assertEquals(count, expectedMetrics.size(), "metrics count is not equal to the expected metrics count.");
+    }
+
+    @Test
+    public void testPrometheusDisabled() throws Exception {
+        final String balFile = Paths.get(RESOURCES_DIR.getAbsolutePath(), "01_http_svc_test.bal").toFile()
+                .getAbsolutePath();
+        serverInstance.startServer(balFile, null, null, new int[] { 9091 });
+
+        String responseData = HttpClientRequest.doGet(TEST_RESOURCE_URL).getData();
+        Assert.assertEquals(responseData, "Sum: 53");
     }
 }
